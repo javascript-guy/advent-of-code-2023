9079f637d52d0d94c45bab7e88c99e8a4c670732
Jared Clayborn <jclayborn@averoinc.com>
2023-12-05 09:07:41 -0600

    add day 4

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const puzzleThree = require("./day2/puzzleThree");
 const puzzleFour = require("./day2/puzzleFour");
 const puzzleFive = require("./day3/puzzleFive");
 const puzzleSix = require("./day3/puzzleSix");
+const puzzleSeven = require("./day4/puzzleSeven");
+const puzzleEight = require("./day4/puzzleEight");
 
 console.log("puzzleOne: ", puzzleOne.puzzleOne());
 console.log("puzzleTwo: ", puzzleTwo.puzzleTwo());
@@ -11,3 +13,5 @@ console.log("puzzleThree: ", puzzleThree.puzzleThree());
 console.log("puzzleFour: ", puzzleFour.puzzleFour());
 console.log("puzzleFive: ", puzzleFive.puzzleFive());
 console.log("puzzleSix: ", puzzleSix.puzzleSix());
+console.log("puzzleSeven: ", puzzleSeven.puzzleSeven());
+console.log("puzzleEight: ", puzzleEight.puzzleEight());
